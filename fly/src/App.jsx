4ce7b2b3b1229d5fce5517624a63324d9cc620fa
Shruti Gupta <202151151@iiitvadodara.ac.in>
2024-01-20 23:41:34 +0530

    All screens completed(small screens)

--- fly/src/App.jsx
+++ fly/src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
       <Lounge/>
       <Travellers/>
       <Subscribers/>
-      {/* <Footer/> */}
+      <Footer/>
     </div>
   )
 }
--- fly/src/Components/Footer/Footer.jsx
+++ fly/src/Components/Footer/Footer.jsx
@@ -1,8 +1,79 @@
 import React from 'react'
 
+//import logo
+import logo from '../../assets/logo.png'
+import { TiSocialFacebook } from 'react-icons/ti'
+import { AiOutlineTwitter } from 'react-icons/ai'
+import { AiFillYoutube } from 'react-icons/ai'
+import { FaPinterestP } from 'react-icons/fa'
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className="sectionContainer container grid">
+        <div className="gridOne">
+          <div className="logoDiv">
+            <img src={logo} className='logo'/>
+          </div>
+          <p>Your mind should be stronger than your feelings, fly!</p>
+          <div className="socialIcon flex">
+            <TiSocialFacebook className='icon'/>
+            <AiOutlineTwitter className='icon'/>
+            <AiFillYoutube className='icon'/>
+            <FaPinterestP className='icon'/>
+          </div>
+        </div>
+
+        <div className="footerLinks">
+          <span className="linkTitle">Information</span>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Explore</a>
+          </li>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Flight Status</a>
+          </li>
+          <li>
+            <a href="">Travel</a>
+          </li>
+          <li>
+            <a href="">Manage your Booking</a>
+          </li>
+        </div>
+        <div className="footerLinks">
+          <span className="linkTitle">Quick Guide</span>
+          <li>
+            <a href="">FAQ</a>
+          </li>
+          <li>
+            <a href="">Features</a>
+          </li>
+          <li>
+            <a href="">Baggage</a>
+          </li>
+          <li>
+            <a href="">Flight Status</a>
+          </li>
+          <li>
+            <a href="">Route Map</a>
+          </li>
+          <li>
+            <a href="">Our Communities</a>
+          </li>
+        </div>
+      </div>
+
+      <div className="copyRightDiv flex">
+        <p>
+          © 2021 Fly. Developed by <a href="" target='_blank'>Shruti Gupta</a>
+        </p>
+      </div>
+    </div>
   )
 }
 
